7b66aef50b2e5e6c4d6d42608984d8d758290b7a
Havran <vavra.radovan@seznam.cz>
2026-03-22 22:28:02 +0100

    Update tennis_scout_live.js

--- tennis_scout_live.js
+++ tennis_scout_live.js
@@ -987,7 +987,7 @@ function buildMatchesTab(sh){
     return data.matches||[];
   }
 
-  function _pf(n){if(!window._pfC){window._pfC={};(window.ATP_PLAYERS||[]).forEach(function(p){var s=p.name.split(' ').slice(1).join(' ').toLowerCase();if(s)window._pfC[s]=p.country;});}var s=(n||'').split(' ')[0].toLowerCase();return FLAGS[window._pfC[s]||'']||'';}
+  function _pf(n){if(!window._pfC){window._pfC={};(window.ATP_PLAYERS||[]).forEach(function(p){var sn=p.name.split(' ').slice(1).join(' ').toLowerCase();if(sn)window._pfC[sn]=FLAGS[p.country]||'';});}var s=(n||'').split(' ')[0].toLowerCase();return window._pfC[s]||'';}
 function renderMatches(data){
     var all=getMatches(data);
     var mcEl=sh.getElementById('nav-matches-count');
