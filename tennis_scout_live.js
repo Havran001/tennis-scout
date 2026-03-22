d8d5b539f2a6a550eb649754497ca0603d15b08c
Havran <vavra.radovan@seznam.cz>
2026-03-22 15:54:27 +0100

    feat: replace green play button with flashscore.svg

--- tennis_scout_live.js
+++ tennis_scout_live.js
@@ -1070,8 +1070,8 @@ function buildMatchesTab(sh){
             }
           }
           h+='</div>';
-          h+='<a href="'+m.url+'" target="_blank" onclick="event.stopPropagation()" title="Flashscore" style="flex-shrink:0;margin:0 8px;width:28px;height:28px;border-radius:7px;background:#00C957;display:flex;align-items:center;justify-content:center;text-decoration:none;">';
-          h+='<span style="width:0;height:0;border-style:solid;border-width:5px 0 5px 9px;border-color:transparent transparent transparent white;margin-left:2px;display:block;"></span></a>';
+          h+='<a href="'+m.url+'" target="_blank" onclick="event.stopPropagation()" title="Flashscore" style="flex-shrink:0;margin:0 8px;width:28px;height:28px;border-radius:7px;overflow:hidden;display:block;text-decoration:none;">';
+          h+='<img src="flashscore.svg" width="28" height="28"/></a>';
           h+='</div></div>';
         });
       });
