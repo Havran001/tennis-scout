7f32ca7037b401e1df6fa5819c400abcc0bfdc60
Havran <vavra.radovan@seznam.cz>
2026-04-06 16:54:46 +0200

    Add files via upload

--- tennis_scout_live.js
+++ tennis_scout_live.js
@@ -2382,7 +2382,7 @@ var _runChance=function(){
       _chanceOdds=d;
       if(!_chanceBaseOdds){_chanceBaseOdds=d;try{localStorage.setItem('ts_chance_base',JSON.stringify(d));}catch(e){}}
       _chanceUpdated=new Date().toLocaleTimeString('cs-CZ',{hour:'2-digit',minute:'2-digit'});
-      if(sh&&sh._renderMatches&&typeof _lastData!=='undefined'&&_lastData)sh._renderMatches(_lastData);
+      if(typeof _lastData!=='undefined'&&_lastData)renderMatches(_lastData);
     }).catch(function(){});
     return;
   }
